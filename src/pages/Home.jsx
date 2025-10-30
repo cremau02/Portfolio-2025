57b4ae68876138b2fd61cbe2e8756a63a4d245f8
Corentin <corentin.remaud@raccourci.fr>
2025-10-30 14:58:49 +0100

    Init Portfolio Project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const Home = () => {
+    return (
+        <div id="pageHome">
+            <p id="title">Me</p>
+            <p id="title">My Works</p>
+            <p id="title">Social Media</p>
+        </div>
+    )
+}
+
+export default Home
