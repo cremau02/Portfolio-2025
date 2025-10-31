b2ef5a344c4054a75825770d356fa51ba6bc9005
Corentin <corentin.remaud@raccourci.fr>
2025-10-31 11:49:27 +0100

    Add features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,48 @@
 import './home.css'
-
+import React from "react";
+import Starter from "../starter/Starter.jsx";
+import { useState } from "react";
 const Home = () => {
 
-    document.querySelectorAll('.title.M').forEach(el => {
-        el.addEventListener('click', () => {
-            console.log('click');
-        });
-    });
-    /**<div className="ghost"></div>*/
+    const [showStarter, setShowStarter] = useState(false);
+
+    if (showStarter) {
+        return <Starter />;
+    }
+
+
     return (
         <div className="page">
             <div className="pageHomeTop">
             </div>
             <div className="pageHomeBar">
                 <div id="titleList">
-                    <p className="title M">Me</p>
-                    <p className="title MW">My Works</p>
-                    <p className="title SM">Social Media</p>
                     <div className="wave-container">
-                        <h1 className="wave-text title">
+                        <h1 className="wave-text1 title">
                             <span>M</span><span>E</span>
                         </h1>
                     </div>
-
+                    <div className="wave-container">
+                        <h1 className="wave-text2 title">
+                            <span>M</span><span>Y</span>
+                        </h1>
+                        <h1 className="wave-text2 title">
+                            <span>W</span><span>O</span><span>R</span><span>K</span><span>S</span>
+                        </h1>
+                    </div>
                 </div>
 
             </div>
 
             <div className="pageHomeBottom">
-
+                <div className="logo">
+                    <div className="logo-display" onClick={() => setShowStarter(true)}>
+                        <h1 className="logo-text title" >&lt;</h1>
+                        <h1 className="logo-slash title">/</h1>
+                        <h1 className="logo-text title">&gt;</h1>
+                    </div>
+                    <div className="logo-extension"></div>
+                </div>
             </div>
             <div className="ghost"></div>
         </div>
