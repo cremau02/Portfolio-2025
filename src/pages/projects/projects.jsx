1a16597b8008985a0d99be50a241a11c1f397271
Corentin <corentin.remaud@raccourci.fr>
2025-11-24 17:47:33 +0100

    new version of projects choices

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -5,50 +5,24 @@ import {useEffect, useState} from "react";
 const Projects = () => {
 
     const projectData = [
-        { id: 1, title: 'Projet 1' },
-        { id: 2, title: 'Projet 2' },
-        { id: 3, title: 'Projet 3' },
+        { id: 1, title: 'Projet 1'},
+        { id: 2, title: 'Projet 2'},
     ];
 
-    const initialState = {
-        left: 1,
-        main : 2,
-        right: 3,
-    };
-
-    const [position, setPosition] = useState(initialState);
-
-    document.addEventListener('scroll', () => console.log(window.scrollY))
+    const initialState = { main: null };
     const logo = document.getElementsByClassName('logo-box');
-
+    const [position, setPosition] = useState(initialState);
+    document.addEventListener('scroll', () => console.log(window.scrollY))
     const getPosition = (id) => {
         return Object.keys(position).find(key => position[key] === id)
     }
 
-
+    console.log(position.main)
     const handleClick = (clickedId) => {
-        if (clickedId.position === 'main') {
+        if (position.id === 'main') {
             return ;
         }
-        const clicked = getPosition(clickedId)
-        console.log(clicked)
-        if (clicked === 'left') {
-            const newPositions = {
-                main: position.left,
-                right: position.main,
-                left: position.right,
-            };
-            setPosition(newPositions);
-        }
-
-        if (clicked === 'right') {
-            const newPositions = {
-                main: position.right,
-                left: position.main,
-                right: position.left,
-            };
-            setPosition(newPositions);
-        }
+        setPosition({ main: clickedId });
     }
 
     return (
@@ -61,13 +35,21 @@ const Projects = () => {
           </div>
         </div>
         <div className="section1">
-            <div className="listProject">
-                {projectData.map((project) => (
-                    <div className={`project ${getPosition(project.id)}`} onClick={() => handleClick(project.id)} ></div>
-                ) )}
+        </div>
+        <div className="section2">
+            <div className="project-starter">
+                <div className="listProject">
+                    {projectData.map((project) => (
+
+                            <div className={`project ${getPosition(project.id)}`} onClick={() => handleClick(project.id)} ></div>
+
+                    ) )}
+                    </div>
+                <div className="project-choice-title">
+                    <p>{position.main ? undefined : "select a project."}</p>
+                </div>
             </div>
         </div>
-        <div className="section2"></div>
       </div>
   )
 };
